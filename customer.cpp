3f67f12b0d4798b06dcc2cdc298d9bcc8ca426b9
Chirag Sareen <chiragsareen8054@gmail.com>
2024-08-13 09:43:36 +0530

    made some classes

--- customer.cpp
+++ customer.cpp
@@ -1,1 +1,15 @@
+#include "customer.h"
 
+Customer::Customer(string n) : name(n) {}
+
+void Customer::bookTicket(const Movie& movie) {
+    bookings.push_back(movie);
+    cout << name << " booked a ticket for " << movie.getTitle() << "\n";
+}
+
+void Customer::displayBookings() const {
+    cout << "Bookings for " << name << ":\n";
+    for (const auto& booking : bookings) {
+        booking.display();
+    }
+}
--- movies.cpp
+++ movies.cpp
@@ -1,2 +1,11 @@
+#include "movie.h"
 
+Movie::Movie(string t, string g, int d) : title(t), genre(g), duration(d) {}
 
+void Movie::display() const {
+    cout << "Movie: " << title << " (" << genre << ") - " << duration << " minutes\n";
+}
+
+string Movie::getTitle() const {
+    return title;
+}
