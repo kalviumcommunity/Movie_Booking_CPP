5ff242e2bf23abe8694ed8844fdaa359db8a9c4a
Chirag Sareen <chiragsareen8054@gmail.com>
2024-08-18 19:56:06 +0530

    made an array of objects

--- main.cpp
+++ main.cpp
@@ -2,18 +2,23 @@
 #include "customer.h"
 
 int main() {
+    Movie movies[] = {
+        Movie("Inception", "Sci-Fi", 148),
+        Movie("The Godfather", "Crime", 175),
+        Movie("The Dark Knight", "Action", 152)
+    };
 
-    Movie movie1("Inception", "Sci-Fi", 148);
-    Movie movie2("The Godfather", "Crime", 175);
-
-    movie1.display();
-    movie2.display();
+    for (const auto& movie : movies) {
+        movie.display();
+    }
 
     Customer customer("John Doe");
-    customer.bookTicket(movie1);
-    customer.bookTicket(movie2);
+
+    customer.bookTicket(movies[0]);
+    customer.bookTicket(movies[1]);
 
     customer.displayBookings();
 
     return 0;
 }
+
