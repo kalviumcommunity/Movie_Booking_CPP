1d07f287a38534736f312dfd735a1e7b9565ed18
Chirag Sareen <chiragsareen8054@gmail.com>
2024-10-07 15:42:23 +0530

    updated code with encapsulation

--- customer.cpp
+++ customer.cpp
@@ -3,19 +3,29 @@
 
 int Customer::customerCounts = 0;
 
-Customer::Customer(string n) : name(n) {
+Customer::Customer(std::string n) : name(n) {
     customerCounts++;
+}
+
+
+std::string Customer::getName() const {
+    return name;
+}
+
+
+void Customer::setName(const std::string& n) {
+    name = n;
 }
 
 void Customer::bookTicket(const Movie& movie) {
     bookings.push_back(movie);
-    cout << name << " booked a ticket for " << movie.getTitle() << "\n";
+    std::cout << name << " booked a ticket for " << movie.getTitle() << "\n";
 }
 
 void Customer::displayBookings() const {
-    cout << "Bookings for " << name << ":\n";
-    for (const auto& booking : bookings) {
-        booking.display();
+    std::cout << "Bookings for " << name << ":\n";
+    for (std::vector<Movie>::const_iterator it = bookings.begin(); it != bookings.end(); ++it) {
+        it->display();
     }
 }
 
--- movies.cpp
+++ movies.cpp
@@ -2,16 +2,37 @@
 
 int Movie::movieCounts = 0;
 
-Movie::Movie(string t, string g, int d) : title(t), genre(g), duration(d) {
+Movie::Movie(std::string t, std::string g, int d) : title(t), genre(g), duration(d) {
     movieCounts++;
 }
 
-void Movie::display() const {
-    cout << "Movie: " << title << " (" << genre << ") - " << duration << " minutes\n";
+
+std::string Movie::getTitle() const {
+    return title;
 }
 
-string Movie::getTitle() const {
-    return title;
+std::string Movie::getGenre() const {
+    return genre;
+}
+
+int Movie::getDuration() const {
+    return duration;
+}
+
+void Movie::setTitle(const std::string& t) {
+    title = t;
+}
+
+void Movie::setGenre(const std::string& g) {
+    genre = g;
+}
+
+void Movie::setDuration(int d) {
+    duration = d;
+}
+
+void Movie::display() const {
+    std::cout << "Movie: " << title << " (" << genre << ") - " << duration << " minutes\n";
 }
 
 int Movie::getMovieCounts() {
